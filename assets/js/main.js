5709029b635924ced4fb4c7ddff64de480765dd7
Tuffu <lucaspaes55@gmail.com>
2024-01-18 19:46:39 -0300

    Criação do front dos status do pokémon

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ let offset = 0;
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
+            <span class="number">#${pokemon.height}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
@@ -16,8 +16,7 @@ function convertPokemonToLi(pokemon) {
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                <a href="pokeStatus.html" target="_self"><img src="${pokemon.photo}" alt="${pokemon.name}"></a>
             </div>
         </li>
     `
